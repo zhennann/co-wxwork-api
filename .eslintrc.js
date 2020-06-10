6d727fb599075543c48c50b711a7d8389149e9a4
zhennann <zhen.nann@icloud.com>
2020-06-10 11:58:54 +0800

    refactor: support appName

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: 'eslint-config-egg',
+  parserOptions: {
+    // ecmaVersion: 6, // 指定ECMAScript支持的版本，6为ES6
+    sourceType: 'module',
+  },
+  // add your custom rules here
+  rules: {
+  },
+  globals: {
+  },
+};
