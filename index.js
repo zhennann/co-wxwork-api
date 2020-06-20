c5b7bdda223f94ef0322bce588ea34686b5a7b88
zhennann <zhen.nann@icloud.com>
2020-06-20 17:33:09 +0800

    feat: sendMessage

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Ticket = require('./lib/core/ticket')
-const AccessToken = require('./lib/core/access-token')
+const Ticket = require('./lib/core/ticket');
+const AccessToken = require('./lib/core/access-token');
 
-const API = require('./lib/core/api')
-const CorpAPI = require('./lib/core/corp-api')
-const SuiteAPI = require('./lib/core/suite-api')
-const SuiteCorpAPI = require('./lib/core/suite-corp-api')
-const ProviderAPI = require('./lib/core/provider-api')
+const API = require('./lib/core/api');
+const CorpAPI = require('./lib/core/corp-api');
+const SuiteAPI = require('./lib/core/suite-api');
+const SuiteCorpAPI = require('./lib/core/suite-corp-api');
+const ProviderAPI = require('./lib/core/provider-api');
 
 /**
  * 用于支持对象合并。将对象合并到API.prototype上，使得能够支持扩展
@@ -16,39 +16,41 @@ const ProviderAPI = require('./lib/core/provider-api')
  * ```
  * @param {Object} obj 要合并的对象
  */
-function mixin (API, obj) {
-  for (let key in obj) {
+function mixin(API, obj) {
+  for (const key in obj) {
     if (API.prototype.hasOwnProperty(key)) {
-      throw new Error('Don\'t allow override existed prototype method. method: ' + key)
+      throw new Error('Don\'t allow override existed prototype method. method: ' + key);
     }
-    API.prototype[key] = obj[key]
+    API.prototype[key] = obj[key];
   }
 }
 
 // crypto
-mixin(API, require('./lib/api_crypto'))
+mixin(API, require('./lib/api_crypto'));
 
 /*  微信企业应用 */
 // 授权
-mixin(CorpAPI, require('./lib/corp_oauth'))
+mixin(CorpAPI, require('./lib/corp_oauth'));
 // JS SDK
-mixin(CorpAPI, require('./lib/corp_js'))
+mixin(CorpAPI, require('./lib/corp_js'));
 // 成员管理
-mixin(CorpAPI, require('./lib/corp_user'))
+mixin(CorpAPI, require('./lib/corp_user'));
 // 部门管理
-mixin(CorpAPI, require('./lib/corp_department'))
+mixin(CorpAPI, require('./lib/corp_department'));
 // 标签管理
-mixin(CorpAPI, require('./lib/corp_tag'))
+mixin(CorpAPI, require('./lib/corp_tag'));
 // 异步任务
-mixin(CorpAPI, require('./lib/corp_batch'))
+mixin(CorpAPI, require('./lib/corp_batch'));
+// 消息推送
+mixin(CorpAPI, require('./lib/corp_message'));
 
 /* 微信企业套件 */
 // 第三方应用
-mixin(SuiteAPI, require('./lib/suite_3rd'))
+mixin(SuiteAPI, require('./lib/suite_3rd'));
 
 /* 微信企业服务商 */
 // 商户授权
-mixin(ProviderAPI, require('./lib/provider_oauth'))
+mixin(ProviderAPI, require('./lib/provider_oauth'));
 
 module.exports = {
   Ticket,
@@ -58,5 +60,5 @@ module.exports = {
   SuiteAPI,
   SuiteCorpAPI,
   ProviderAPI,
-  mixin
-}
+  mixin,
+};
